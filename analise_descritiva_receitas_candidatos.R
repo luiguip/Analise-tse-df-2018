fb767cd66684c2209ac467d4a4adbde6dfbad8c4
luiguip <luisugilhermepdea@gmail.com>
2019-11-14 17:42:11 -0300

    14/11

--- analise_descritiva_receitas_candidatos.R
+++ analise_descritiva_receitas_candidatos.R
@@ -1,6 +1,9 @@
 #Retira notação científica
 options(scipen=10000)
-#Analizar receita por descricao_receita
-ggplot(receitas_candidatos, aes(x=DS_RECEITA, y=VR_RECEITA)) + geom_col()
+#Analizar receita por fundo receita
+ggplot(receitas_candidatos, aes(x=factor(DS_FONTE_RECEITA), y=VR_RECEITA)) +
+  geom_bar(stat='identity', fill='steelblue') +
+  labs(title="Receita por Fundo", y="Receita", x="Fonte Receita", caption="Fonte: Própria")
 #Analizar receita por partido
-ggplot(receitas_candidatos, aes(x=SG_PARTIDO, y=VR_RECEITA)) + geom_col()
+ggplot(receitas_candidatos, aes(x=SG_PARTIDO, y=VR_RECEITA)) + geom_col(color='skyblue',fill='steelblue')
+#5 partidos que tiveram mais receita
