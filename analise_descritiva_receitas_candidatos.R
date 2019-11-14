621af700ec0a5f73096b06ad143fc8e3ab364a81
luiguip <luisugilhermepdea@gmail.com>
2019-11-14 17:04:33 -0300

    corecao

--- analise_descritiva_receitas_candidatos.R
+++ analise_descritiva_receitas_candidatos.R
@@ -1,6 +1,6 @@
 #Retira notação científica
 options(scipen=10000)
-#Analizar receita por sexo
-ggplot(receitas_candidatos, aes(x=))
+#Analizar receita por descricao_receita
+ggplot(receitas_candidatos, aes(x=DS_RECEITA, y=VR_RECEITA)) + geom_col()
 #Analizar receita por partido
 ggplot(receitas_candidatos, aes(x=SG_PARTIDO, y=VR_RECEITA)) + geom_col()
