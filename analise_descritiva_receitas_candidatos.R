e0e79135542b619c24d995f9ee68780654e82854
luiguip <luisugilhermepdea@gmail.com>
2019-11-16 14:27:20 -0300

    join

--- analise_descritiva_receitas_candidatos.R
+++ analise_descritiva_receitas_candidatos.R
@@ -1,9 +1,20 @@
 #Retira notação científica
 options(scipen=10000)
+
+#Candidatos por partido
+ggplot(receita_por_candidato, aes(reorder(SG_PARTIDO, SG_PARTIDO, function(x) - length(x)))) +
+    geom_bar() +
+    labs(title="Candidatos por Partido", y="Quantidade de candidatos", x="Partidos", caption = "Fonte: Pŕopria")
+#Receita por partido
+ggplot(receita_por_partido, aes(x=reorder(SG_PARTIDO, -receita_partido), y=receita_partido)) +
+    geom_col() +
+  labs(title="Receita por Partido", y="Receita", x="Partidos", caption = "Fonte: Pŕopria")
+#Despesa por partido
+
 #Analizar receita por fundo receita
 ggplot(receitas_candidatos, aes(x=factor(DS_FONTE_RECEITA), y=VR_RECEITA)) +
-  geom_bar(stat='identity', fill='steelblue') +
-  labs(title="Receita por Fundo", y="Receita", x="Fonte Receita", caption="Fonte: Própria")
+    geom_bar(stat='identity', fill='steelblue') +
+    labs(title="Receita por Fundo", y="Receita", x="Fonte Receita", caption="Fonte: Própria")
 #Analizar receita por partido
-ggplot(receitas_candidatos, aes(x=SG_PARTIDO, y=VR_RECEITA)) + geom_col(color='skyblue',fill='steelblue')
+ggplot(receita_por_partido, aes(x=reorder(SG_PARTIDO, -receita_partido), y=receita_partido)) + geom_col(color='skyblue',fill='steelblue')
 #5 partidos que tiveram mais receita
--- importar_arquivos.R
+++ importar_arquivos.R
@@ -10,3 +10,25 @@ path <- '/home/luigui/Documents/data-science/julio2/'
 
 receitas_candidatos <- import_csv(path, 'receitas_candidatos_2018_DF.csv')
 despesas_contratadas <- import_csv(path, 'despesas_contratadas_candidatos_2018_DF.csv')
+
+receita_por_partido <- receitas_candidatos %>%
+  group_by(SG_PARTIDO) %>%
+  summarise(receita_partido = sum(VR_RECEITA))
+
+receita_por_candidato <- receitas_candidatos %>%
+  group_by(NM_CANDIDATO, SG_PARTIDO) %>%
+  summarise(receita_candidato = sum(VR_RECEITA))
+
+despesa_por_partido <- despesas_contratadas %>%
+  group_by(SG_PARTIDO) %>%
+  summarise(despesa_partido = sum(VR_DESPESA_CONTRATADA))
+
+despesa_por_candidato <- despesas_contratadas %>%
+  group_by(NM_CANDIDATO, SG_PARTIDO) %>%
+  summarise(despesa_candidato = sum(VR_DESPESA_CONTRATADA))
+
+saldo_por_partido <- receita_por_partido %>%
+  inner_join(despesa_por_partido)
+
+saldo_por_candidato <- receita_por_candidato %>%
+  inner_join(despesa_por_candidato)
